30a116bf416b3a1431dd2517eff11db8a49bd86a
Adrian Perea <adrianmarkperea@gmail.com>
2020-11-17 13:53:08 +0800

    Modify deployment script to account for different container names

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t aperea/multi-client:latest -t aperea/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t aperea/multi-server:latest -t aperea/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t aperea/multi-worker:latest -t aperea/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t aperea/multi-server:latest -t aperea/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aperea/multi-worker:latest -t aperea/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push aperea/multi-client:latest
 docker push aperea/multi-server:latest
@@ -12,5 +12,5 @@ docker push aperea/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=aperea/multi-server:$SHA
-kubectl set image deployments/client-deployment server=aperea/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=aperea/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=aperea/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aperea/multi-worker:$SHA
